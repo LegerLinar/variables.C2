130a92fcc4f30125af4c208c29a43e5b5da51058
pavelbesedin <pavelbesedin97@mail.ru>
2023-03-15 15:09:28 +0300

    Initial multiply numbers variables

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,30 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        task2();
+//        task2();
     }
 
     public static void task1 () {
         System.out.println("Задача 1");
-//        ... // Пишем код для задачи 1
+
+        byte byteVariable = 1;
+        System.out.println("Значение переменной byteVariable с типом byte равно " + byteVariable);
+        short shortVariable = 100;
+        System.out.println("Значение переменной shortVariable с типом short равно " + shortVariable);
+        int intVariable = 100 * 500;
+        System.out.println("Значение переменной intVariable с типом int равно " + intVariable);
+        long longVariable = 110L;
+        System.out.println("Значение переменной longVariable с типом long равно " + longVariable);
+        float floatVariable = 2.55f;
+        System.out.println("Значение переменной floatVariable с типом float равно " + floatVariable);
+        double doubleVariable = 0.50;
+        System.out.println("Значение переменной doubleVariable с типом double равно " + doubleVariable);
+
+
     }
 
-    public static void task2 () {
-        System.out.println("Задача 2");
-//        ... // Пишем код для задачи 2
-    }
+//    public static void task2 () {
+//        System.out.println("Задача 2");
+////        ... // Пишем код для задачи 2
+//    }
 }
